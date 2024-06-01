665a0c3d65a3a33e9f6b12ce1e6f530fa7858898
MengHanLOVE1027 <2193438288@qq.com>
2024-06-01 21:24:16 +0800

    创建文件夹，开始插件编写之旅~

---
+++ DontHitWhiteBlocks.js
@@ -0,0 +1,34 @@
+// LiteLoader-AIDS automatic generated
+/// <reference path="c:\Users\HeYuHan\LiteDev/dts/helperlib/src/index.d.ts"/>
+
+
+// 注册插件
+const plugin = {
+    Name: "DontHitWhiteBlocks",
+    Introduction: "LeviLamina - 别踩白块小游戏 v0.1.0 作者: 梦涵LOVE",
+    Version: [0, 1, 0],
+    Other: {
+        Author: "梦涵LOVE",
+        Github: "https://github.com/MengHanLOVE1027/DontHitWhiteBlocks",
+        License: "GPL-3.0 license",
+    },
+}
+
+
+
+/**
+ * 全局常量模块
+ * (Start)
+ */
+
+// 声明常量
+const plugin_name = "DontHitWhiteBlocks",
+    plugin_version = "v0.1.0",
+    // cmd_name = "DontHitWhiteBlocks",
+    // cmd_alias = "DontHitWhiteBlocks",
+    plugin_path = `./plugins/${plugin_name}`
+
+/**
+ * 全局常量模块
+ * (End)
+ */
---
+++ README.md
@@ -0,0 +1,24 @@
+# EasyBackuper
+
+简单化的LSE - JS备份插件
+
+## Install
+
+To install all engines as a whole, you can use the following command:
+
+```sh
+lip install github.com/MengHanLOVE1027/EasyBackuper
+```
+
+## Usage
+See https://www.minebbs.com/resources/easybackuper-eb.7771/
+
+## Contributing
+
+If you have any questions, please open an issue to discuss it.
+
+PRs are welcome.
+
+## License
+
+GPL-3.0-or-later © MengHanLOVE1027
